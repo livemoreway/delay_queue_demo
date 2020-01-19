63fae647b49f22ef3e22f6d7376a84dff4eb2bd2
mayben0t <594180536@qq.com>
2020-01-19 10:25:29 +0800

    事物机制

---
+++ src/main/java/zz/ZZ.java
@@ -0,0 +1,41 @@
+package zz;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ReturnListener;
+import conn.ConUtils;
+import pre.Prepare;
+
+import java.io.IOException;
+
+public class ZZ {
+
+
+    public static void main(String[] args) throws Exception{
+        Connection conn = ConUtils.getConn();
+        Channel channel = conn.createChannel();
+        try {
+            channel.txSelect();
+            byte[] bytes = "1秒过期的消息".getBytes();
+            channel.basicPublish(Prepare.exchange1, Prepare.routing_key1, true, null, bytes);
+            byte[] bytes2 = "2秒过期的消息".getBytes();
+            channel.basicPublish(Prepare.exchange1, Prepare.routing_key2, true, null, bytes2);
+            byte[] bytes3 = "3秒过期的消息".getBytes();
+            channel.basicPublish(Prepare.exchange1, Prepare.routing_key3, true, null, bytes3);
+            int i = 1 / 0;
+            channel.txCommit();
+        }catch (Exception ex){
+            channel.txRollback();
+        }
+
+        channel.addReturnListener(new ReturnListener() {
+            @Override
+            public void handleReturn(int replyCode, String replyText, String exchange, String routingKey, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println(exchange+"-"+routingKey+":"+body);
+            }
+        });
+        channel.close();
+        conn.close();
+    }
+}
